a9431b6e5a493be391260e524f3c650c7bd218fb
zdw31-hd <145539001+zdw31-hd@users.noreply.github.com>
2023-11-29 18:57:24 +0000

    first commit

---
+++ tsdev/src/index.ts
@@ -0,0 +1,10 @@
+class Main {
+    static sayHello(n:number) {
+        for (let i=0; i<n; i++) console.log("Hello World");
+    }
+}
+
+Main.sayHello(3);
+Main.sayHello(4);
+console.log("Das funktioniert!");
+console.log("super!");
