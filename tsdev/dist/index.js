7d9561c299eae19a48fdedfdffa954a33363be93
yannick9518 <78787477+yannick9518@users.noreply.github.com>
2023-11-30 09:12:45 +0000

    first commit

---
+++ tsdev/dist/index.js
@@ -0,0 +1,10 @@
+"use strict";
+class Main {
+    static sayHello(m) {
+        for (let i = 0; i < m; i++) {
+            console.log('Hello World');
+        }
+    }
+}
+Main.sayHello(3);
+//# sourceMappingURL=index.js.map
---
+++ tsdev/src/index.ts
@@ -0,0 +1,9 @@
+class Main{
+    static sayHello(m:number){
+        for(let i=0; i<m;i++){
+            console.log('Hello World');
+        }
+    }
+}
+
+Main.sayHello(3);
